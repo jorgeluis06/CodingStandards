b389a2c5f1d2a2c29c739f929ef8d4e371ef9afb
Your NameJorge <jorgeluis993@hotmail.com>
2021-06-03 23:59:14 -0500

    Taller Coding-Standars completado

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,5 +1,5 @@
-//
-//
+// Copyright (C) 2020
+// All rights reserved
 
 package labcodingstandards;
 
@@ -7,18 +7,23 @@ import java.util.Scanner;
 
 
 /**
- * @author
+ * @author Jorge Rodriguez
  *
  */
 public class Calculator {	
-	
+	//CHECKSTYLE: OFF
+	/**
+	* Programa principal
+	* @param args : argumentos
+	*/
 	public static void main(String[] args) {
+	//CHECKSTYLE: ON
 		Scanner reader = new Scanner(System.in);
 		
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
         
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
         
@@ -27,7 +32,7 @@ public class Calculator {
         	input = reader.nextLine();
         	
             try {
-            	First=Integer.parseInt(input);
+            	first=Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
@@ -47,13 +52,20 @@ public class Calculator {
         }
 
         Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+        String result=cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
-
-	private String Operation(double first,double second,char operator) {
+	
+	/**
+	* Un metodo de operacion
+	* @param first : numbers
+	* @param second : numbers
+	* @param operator : symbols
+	* @return String
+	*/
+	private String operation(double first, double second, char operator) {
 		double result = 0;
 		switch(operator)
         {
@@ -72,6 +84,6 @@ public class Calculator {
             default:
             	return "Error! operator is not correct";
         }
-		return "The result is: "+result;
+				return "The result is: "+result;
 	}
 }
